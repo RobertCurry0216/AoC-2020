3e3c23107fdce1a678dcb56255541b91d5bdbe32
Robert Curry <robertcury0216@gmail.com>
2020-12-02 17:30:04 +1100

    leaving work commit

---
+++ AdventOfCode/Solutions/Day02.cs
@@ -0,0 +1,36 @@
+﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Day02 : BaseDay
+    {
+        private readonly List<string> _input;
+
+        public Day02()
+        {
+            _input = File.ReadAllLines(InputFilePath).ToList();
+        }
+
+        public override string Solve_1()
+        {
+            var count = 0;
+            foreach (var input in _input)
+            {
+                
+            }
+            return $"{count}";
+        }
+
+        public override string Solve_2()
+        {
+            
+            return $"n/a";
+        }
+    }
+}
