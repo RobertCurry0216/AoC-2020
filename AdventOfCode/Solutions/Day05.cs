18cb7770c79f7d1d3f7b83dc758084618e250a18
RobertCurry <robertcurry0216@gmail.com>
2020-12-06 09:57:52 +1100

    day5 done

--- AdventOfCode/Solutions/Day05.cs
+++ AdventOfCode/Solutions/Day05.cs
@@ -18,16 +18,71 @@ namespace AdventOfCode
             _input = File.ReadAllLines(InputFilePath).ToList();
         }
 
+        private (int row, int col) ScanBoardingPass(string code)
+        {
+            int row = 0;
+            int col = 0;
+
+            int max = 127;
+            int min = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                if (code[i] == 'F')
+                {
+                    max = (max + min) / 2;
+                }
+                else
+                {
+                    min = (max + min) / 2;
+                }
+            }
+            row = max;
+
+            max = 7;
+            min = 0;
+            for (int i = 7; i < 10; i++)
+            {
+                if (code[i] == 'L')
+                {
+                    max = (max + min) / 2;
+                }
+                else
+                {
+                    min = (max + min) / 2;
+                }
+            }
+            col = max;
+
+            return (row, col);
+        }
+
+        private int SeatId((int row, int col) seat) => (seat.row * 8) + seat.col;
+
         public override string Solve_1()
         {
-            var count = 0;
-            return $"{count}";
+            var highest = 0;
+
+            _input.ForEach(l =>
+            {
+                var seat = ScanBoardingPass(l);
+                highest = Math.Max(highest, SeatId(seat));
+            });
+
+            return $"{highest}";
         }
 
         public override string Solve_2()
         {
-            var count = 0;
-            return $"{count}";
+            var seatIds = _input.Select(s => SeatId(ScanBoardingPass(s))).ToList();
+            seatIds.Sort();
+
+            for (int i = 0; i < seatIds.Count-1; i++)
+            {
+                if (seatIds[i + 1] - seatIds[i] != 2) continue;
+                return $"{seatIds[i] + 1}";
+            }
+
+            return $"{-1}";
         }
     }
 }
