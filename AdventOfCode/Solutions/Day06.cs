4bacdb2ee1f15febf5596508e91088980aae7cae
Robert <robertcurry0216@gmail.com>
2020-12-06 22:45:47 +1100

    day6 done

--- AdventOfCode/Solutions/Day06.cs
+++ AdventOfCode/Solutions/Day06.cs
@@ -11,22 +11,50 @@ namespace AdventOfCode
 {
     class Day06 : BaseDay
     {
-        private readonly List<string> _input;
+        private readonly List<List<string>> _input;
 
         public Day06()
         {
-            _input = File.ReadAllLines(InputFilePath).ToList();
+            _input = new List<List<string>>();
+            var lineGroup = new List<string>();
+            foreach (string line in File.ReadAllLines(InputFilePath))
+            {
+                if(string.IsNullOrEmpty(line))
+                {
+                    _input.Add(lineGroup);
+                    lineGroup = new List<string>();
+                    continue;
+                }
+                lineGroup.Add(line.Trim());
+            }
+            _input.Add(lineGroup);
         }
 
         public override string Solve_1()
         {
             var count = 0;
+
+            count = _input.Aggregate(0, (acc, group) =>
+            {
+                var s = new HashSet<char>();
+                group.ForEach(l => s.UnionWith(l.ToCharArray()));
+                return acc + s.Count;
+            });
+
             return $"{count}";
         }
 
         public override string Solve_2()
         {
             var count = 0;
+
+            count = _input.Aggregate(0, (acc, group) =>
+            {
+                var m = new Dictionary<char, int>();
+                group.ForEach(line => line.ToCharArray().ToList().ForEach(c => m[c] = m.ContainsKey(c) ? m[c]+ 1 : 1));
+                return acc + m.Where(kvp => kvp.Value == group.Count).Count();
+            });
+
             return $"{count}";
         }
     }
