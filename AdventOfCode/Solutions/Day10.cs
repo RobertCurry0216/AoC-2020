fd31ccccb21f5fd9eabe2de2c07d0139c5594228
Robert Curry <robertcury0216@gmail.com>
2020-12-11 10:42:28 +1100

    day10 in progress

--- AdventOfCode/Solutions/Day10.cs
+++ AdventOfCode/Solutions/Day10.cs
@@ -11,23 +11,59 @@ namespace AdventOfCode
 {
     class Day10 : BaseDay
     {
-        private readonly List<string> _input;
+        private readonly List<int> _input;
 
         public Day10()
         {
-            _input = File.ReadAllLines(InputFilePath).ToList();
+            _input = File.ReadAllLines(InputFilePath).Select(i => int.Parse(i)).ToList();
         }
 
         public override string Solve_1()
         {
-            var count = 0;
-            return $"{count}";
+            var count1 = 0;
+            var count3 = 0;
+
+            var jolts = new List<int>(_input);
+            jolts.Add(0);
+            jolts.Sort();
+            for (int i = 1; i < jolts.Count; i++)
+            {
+                var diff = jolts[i] - jolts[i - 1];
+                if (diff == 1) count1++;
+                if (diff == 3) count3++;
+            }
+            count3++;
+            return $"{count1 * count3}";
         }
 
         public override string Solve_2()
         {
-            var count = 0;
+            long count;
+
+            var jolts = new List<int>(_input);
+            jolts.Sort();
+
+            count = GetChargerArrangements(0, jolts, new long[jolts.Count]);
+
             return $"{count}";
+        }
+
+        private long GetChargerArrangements(int idx, List<int> jolts, long[] cache)
+        {
+            if (cache[idx] != 0) return cache[idx];
+            if (idx >= jolts.Count - 1) return 1;
+
+            var value = jolts[idx];
+            long count = 0;
+
+            for (int i = idx + 1; i < Math.Min(jolts.Count, idx + 4); i++)
+            {
+                if (jolts[i] - value <= 3) count += GetChargerArrangements(i, jolts, cache);
+            }
+
+            cache[idx] = count;
+
+            return count;
         }
     }
 }
